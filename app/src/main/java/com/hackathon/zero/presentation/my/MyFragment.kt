5980bdd3d05a7f5245da17edbc0c14e956b8800f
hangunhee39 <ghee0831@naver.com>
2023-11-26 08:50:59 +0900

    feat : f

--- app/src/main/java/com/hackathon/zero/presentation/my/MyFragment.kt
+++ app/src/main/java/com/hackathon/zero/presentation/my/MyFragment.kt
@@ -20,9 +20,8 @@ class MyFragment : BaseFragment<FragmentMyBinding>(R.layout.fragment_my) {
         super.onViewCreated(view, savedInstanceState)
         binding.RVAdapter.adapter = adapter
         val list = listOf<Product>(
-            Product("제로콜라",0.1,1,250,1,),
-            Product("dd",0.2,2,1,1,),
-            Product("dd",0.1,1,2,1,)
+            Product("코카 콜라",16.0,320,250,49,),
+            Product("아침햇살",5.0,280,100,0,),
         )
         adapter.setList(list)
     }
